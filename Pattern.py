d749fdfa401162812c8e09b4d898480b8e975cb9
andriii-droid <andri.andermatt@icloud.com>
2026-05-20 10:24:39 +0200

    Pattern Doc

--- Pattern.py
+++ Pattern.py
@@ -1,12 +1,13 @@
 from reportlab.lib.pagesizes import A6
 from reportlab.lib import colors
 from reportlab.pdfgen import canvas
-import math
 from Point import Point
 
 
 class Pattern:
     def __init__(self, filename='output', circles=False, lines=False, sketch=False, cord=False):
+        '''Creates a Pattern Instance, which creates a pdf canvas. 
+        It implemets Functions to draw Points and Lines'''
         self.circles = circles
         self.lines = lines
         self.sketch = sketch
@@ -14,11 +15,9 @@ class Pattern:
         self.width, self.height = A6
         self.center = Point(self.width / 2 , self.height / 2)
 
+        self.draw_points([self.center], r=2) #Draw the Center Point
 
-        if self.circles:
-            self.c.circle(*self.center.cartesian, r=3, stroke=0, fill=1)
-
-        if cord:
+        if cord:    #Draw coordinate System
             self.c.setLineWidth(.05)
             self.c.setStrokeColor(colors.gray)
             self.c.circle(*self.center.cartesian, r=50, stroke=1, fill=0)
@@ -32,15 +31,20 @@ class Pattern:
             self.c.line(*starty.cartesian, *endy.cartesian)
 
     def savePDF(self):
+        '''Saves the PDF'''
         self.c.showPage()
         self.c.save()
 
-    def draw_points(self, points):
-        for p1, p2 in zip(points, points[1:]+[points[0]]):
+    def draw_points(self, points, r=1):
+        '''Draws the given Points onto the PDF'''
+        for p1 in points:
             if self.circles:
-                self.c.circle(*p1.cartesian, r=1, stroke=0, fill=1)
+                self.c.circle(*p1.cartesian, r=r, stroke=0, fill=1)
 
     def draw_lines(self, points, angle, offset):
+        '''Draws Lines between the given Points
+        if offset = -1 the Lines are drawn between zhe consecutive points in the array,
+        otherwise offset+1 Points in the array gets skipped'''
         for count, (p1, p2) in enumerate(zip(points, points[offset+2:]+points[0:offset+2])):
             if self.lines or(self.sketch and angle == 0 and not (offset)) or (self.sketch and offset and count == 0):
                 self.c.setLineWidth(.2)
